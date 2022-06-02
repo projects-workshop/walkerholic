48e048376ff863251a2cf970b77747e7665b6aba
yunhalee05 <yunhalee0506@naver.com>
2022-06-02 19:17:25 +0900

    Refactor: 도메인 수정

--- src/main/java/com/yunhalee/walkerholic/config/WebConfig.java
+++ src/main/java/com/yunhalee/walkerholic/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://walkerholic.o-r.kr")
+            .allowedOrigins("https://walkerholic.n-e.kr")
             .allowedOrigins("http://localhost:3000")
             .allowedMethods(
                 HttpMethod.GET.name(),
